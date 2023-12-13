5f42a4f663c781c70c08ba1fe8edf25f892091e9
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2023-12-13 16:53:50 +0000

    Latest ReWrite

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-function closeModal()
-{
-// Get the modal
-var modal = document.getElementById('id01');
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+var accordian = document.getElementById("accordian");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function showAccordian() {
+if (accordian.style.display == 'block') {
+  accordian.style.display = 'none';
+  } else {
+    accordian.style.display = 'block'
   }
-}}
+}
+
+function showAccordianScroll() {
+  accordian.style.display = 'none';
+}
+window.addEventListener("scroll", showAccordianScroll);
